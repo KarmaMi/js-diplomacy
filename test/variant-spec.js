203e459f8923d7fde20201b7b695c945faaf3f64
karmami <karmami674+github@gmail.com>
2016-10-27 18:48:32 +0900

    Fix the implementation of Variant
      ref #5

--- test/variant-spec.js
+++ test/variant-spec.js
@@ -5,11 +5,10 @@ const assert = require('assert')
 
 const Name = require('./../lib/data/name')
 const Location = require('./../lib/data/location')
-const Unit = require('./../lib/data/unit')
 const Edge = require('./../lib/data/edge')
 const DiplomacyMap = require('./../lib/data/diplomacy-map')
+const Variant = require('./../lib/variant/variant')
 const Order = require('./../lib/variant/standard/order')
-const Variant = require('./../lib/variant')
 
 chai.should()
 
@@ -50,20 +49,20 @@ describe('Variant', () => {
       }
     }
 
-    v.$$.Nap().should.deep.equal(nap)
-    v.$$.Spa().should.deep.equal(spa)
-    v.$$.Spa(v.$$.SC).should.deep.equal(spaSc)
+    v.$p.Nap().should.deep.equal(nap)
+    v.$p.Spa().should.deep.equal(spa)
+    v.$p.Spa(v.$k.SC).should.deep.equal(spaSc)
 
-    v.F(v.$$.Nap()).militaryBranch.should.deep.equal(fleet)
-    v.F(v.$$.Nap()).location.should.deep.equal(nap)
+    v.$m.F(v.$p.Nap()).militaryBranch.should.deep.equal(fleet)
+    v.$m.F(v.$p.Nap()).location.should.deep.equal(nap)
 
-    const c = v.F(v.$$.Wes()).convoy(v.A(v.$$.Nap()).move(v.$$.Spa()).viaConvoy())
+    const c = v.F(v.Wes()).convoy(v.A(v.Nap()).move(v.Spa()).viaConvoy())
 
     c.unit.militaryBranch.should.deep.equal(fleet)
-    c.unit.location.should.deep.equal(v.$$.Wes())
+    c.unit.location.should.deep.equal(v.Wes())
     c.target.unit.militaryBranch.should.deep.equal(army)
-    c.target.unit.location.should.deep.equal(v.$$.Nap())
-    c.target.destination.should.deep.equal(v.$$.Spa())
+    c.target.unit.location.should.deep.equal(v.Nap())
+    c.target.destination.should.deep.equal(v.Spa())
     c.target.isViaConvoy.should.equal(true)
   })
 })
