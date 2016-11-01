9e50252ede88631470fa7a2173d6f8684aef7dc8
karmami <karmami674+github@gmail.com>
2016-11-01 13:33:57 +0900

    Add system-helper and map-helper
      ref #7

--- test/variant/map-helper-spec.js
+++ test/variant/map-helper-spec.js
@@ -8,12 +8,11 @@ const Location = require('./../../lib/data/location')
 const Edge = require('./../../lib/data/edge')
 const Province = require('./../../lib/data/province')
 const DiplomacyMap = require('./../../lib/data/diplomacy-map')
-const Helper = require('./../../lib/variant/helper')
-const Order = require('./../../lib/variant/standard/order')
+const MapHelper = require('./../../lib/variant/map-helper')
 
 chai.should()
 
-describe('Helper', () => {
+describe('MapHelper', () => {
   it('defines helper functions.', () => {
     const fleet = new Name('Fleet', 'F')
     const army = new Name('Army', 'A')
@@ -42,25 +41,8 @@ describe('Helper', () => {
       ]
     )
 
-    const orders = []
-    for (const name in Order) {
-      orders.push([name.toLowerCase(), Order[name]])
-    }
-
-    const h = new Helper([], [], [fleet, army], orders, map)
+    const h = new MapHelper([], map)
     h.$l.Nap.should.deep.equal(nap)
     h.$l['Spa/SC'].should.deep.equal(spaSc)
-
-    h.$m.F(h.$l.Nap).militaryBranch.should.deep.equal(fleet)
-    h.$m.F(h.$l.Nap).location.should.deep.equal(nap)
-
-    const c = h.F(h.Wes).convoy(h.A(h.Nap).move(h.Spa).viaConvoy())
-
-    c.unit.militaryBranch.should.deep.equal(fleet)
-    c.unit.location.should.deep.equal(h.Wes)
-    c.target.unit.militaryBranch.should.deep.equal(army)
-    c.target.unit.location.should.deep.equal(h.Nap)
-    c.target.destination.should.deep.equal(h.Spa)
-    c.target.isViaConvoy.should.equal(true)
   })
 })
---
+++ test/variant/system-helper-spec.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const chai = require('chai')
+const assert = require('assert')
+
+const Name = require('./../../lib/data/name')
+const Location = require('./../../lib/data/location')
+const Province = require('./../../lib/data/province')
+const SystemHelper = require('./../../lib/variant/system-helper')
+const Order = require('./../../lib/variant/standard/order')
+
+chai.should()
+
+describe('SystemHelper', () => {
+  it('defines helper functions.', () => {
+    const fleet = new Name('Fleet', 'F')
+    const army = new Name('Army', 'A')
+
+    const spain = new Province(new Name('Spa'), null, true)
+    const spa = new Location(spain, [army])
+    const spaSc = new Location(spain, [fleet])
+    spaSc.toString = () => 'Spa/SC'
+
+    const naples = new Province(new Name('Nap'))
+    const nap = new Location(naples, [army, fleet])
+
+    const western = new Province(new Name('Wes'))
+    const wes = new Location(western, [fleet])
+
+    const orders = []
+    for (const name in Order) {
+      orders.push([name.toLowerCase(), Order[name]])
+    }
+
+    const h = new SystemHelper([], [fleet, army], orders)
+    h.$m.F(nap).militaryBranch.should.deep.equal(fleet)
+    h.$m.F(nap).location.should.deep.equal(nap)
+
+    const c = h.F(wes).convoy(h.A(nap).move(spa).viaConvoy())
+
+    c.unit.militaryBranch.should.deep.equal(fleet)
+    c.unit.location.should.deep.equal(wes)
+    c.target.unit.militaryBranch.should.deep.equal(army)
+    c.target.unit.location.should.deep.equal(nap)
+    c.target.destination.should.deep.equal(spa)
+    c.target.isViaConvoy.should.equal(true)
+  })
+})
