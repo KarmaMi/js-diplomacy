7b373caf8985636dbab0ca446c6671c784a98cbe
karmami <karmami674+github@gmail.com>
2016-11-03 11:13:08 +0900

    Implement deleteNode function
      ref #7

--- test/util/directed-graph-spec.js
+++ test/util/directed-graph-spec.js
@@ -21,4 +21,14 @@ describe('DirectedGraph', () => {
     const g3 = g2.mergeNodes([1, 2, 4]);
     ([...g3.nodes]).should.deep.equal([[1, ['a', 'b', 'c', 'd']]])
   })
+  it('can delete a node.', () => {
+    const g1 = new DirectedGraph(
+      [[1, 'a'], [2, 'b'], [3, 'c'], [4, 'd']],
+      [[1, 2], [2, 3], [3, 2], [3, 4]]
+    )
+    const g2 = g1.deleteNode(2);
+
+    ([...g2.nodes]).should.deep.equal([[1, 'a'], [3, 'c'], [4, 'd']]);
+    ([...g2.edges]).should.deep.equal([[3, 4]])
+  })
 })
