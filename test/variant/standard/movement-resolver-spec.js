d09b7c381d820d5d44329115298376a99840713f
karmami <karmami674+github@gmail.com>
2016-11-07 14:29:17 +0900

    DIAGRAM 20
      ref #7

--- test/variant/standard/movement-resolver-spec.js
+++ test/variant/standard/movement-resolver-spec.js
@@ -567,4 +567,44 @@ describe('MovementResolver', () => {
       )
     )
   })
+  it('DIAGRAM 21', () => {
+    const { board, orderResult } = r.resolve(
+      map,
+      new Board(
+        variant.initialBoard.state,
+        [
+          [$.France, [$.A($.Spa), $.F($.GoL), $.F($.Tyn)]],
+          [$.Italy, [$.F($.Tun), $.F($.Ion)]]
+        ],
+        [], [], []
+      ),
+      [
+        $.A($.Spa).move($.Nap),
+        $.F($.GoL).convoy($.A($.Spa).move($.Nap)), $.F($.Tyn).convoy($.A($.Spa).move($.Nap)),
+        $.F($.Ion).move($.Tyn), $.F($.Tun).support($.F($.Ion).move($.Tyn))
+      ]
+    )
+
+    ResolverSpecUtil.checkOrderResult(
+      orderResult,
+      [
+        [$.F($.GoL).convoy($.A($.Spa).move($.Nap)), $.Success],
+        [$.F($.Tun).support($.F($.Ion).move($.Tyn)), $.Success],
+        [$.F($.Tyn).convoy($.A($.Spa).move($.Nap)), $.Dislodged],
+        [$.F($.Ion).move($.Tyn), $.Success],
+        [$.A($.Spa).move($.Nap), $.Fail]
+      ]
+    )
+    ResolverSpecUtil.checkBoard(
+      board,
+      new Board(
+        new State(1901, $.Spring, $.Retreat),
+        [
+          [$.France, [$.A($.Spa), $.F($.GoL)]],
+          [$.Italy, [$.F($.Tun), $.F($.Tyn)]]
+        ],
+        [], [[$.F($.Tyn), $.Dislodged]], []
+      )
+    )
+  })
 })
