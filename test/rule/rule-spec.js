0280d3bd36844d9c6865007f08fa0ebc54e8eaae
karmami <karmami674+github@gmail.com>
2016-11-10 18:21:08 +0900

    Add getErrorMessageForOrders
      ref #7

--- test/rule/rule-spec.js
+++ test/rule/rule-spec.js
@@ -56,6 +56,7 @@ describe('Rule', () => {
 
       rule._resolveOrder = (map, board, os) => { result = os }
       rule.getErrorMessageForOrder = (map, board, order) => (order.type === 'Hold') ? null : 'invalid'
+      rule.getErrorMessageForOrders = (map, board, orders) => null
       rule.defaultOrder = (map, board, unit) => $.A(unit.location).hold()
 
       rule.resolve(null, { units: [] }, [$.A(mar).move(spa), $.A(spa).hold()])
@@ -63,6 +64,33 @@ describe('Rule', () => {
       result.length.should.equal(2)
       result[0].type.should.equal('Hold')
       result[1].type.should.equal('Hold')
+    })
+  })
+  describe('when orders are invalid', () => {
+    it('returns null', () => {
+      const army = new Name('Army', 'A')
+      const spa_ = new Province(new Name('Spa'), null, true)
+      const spa = new Location(spa_, [army])
+
+      const mar_ = new Province(new Name('Mar'), 'France', true)
+      const mar = new Location(mar_, [army])
+
+      const orders = []
+      for (const name in Order) {
+        orders.push([name.toLowerCase(), Order[name]])
+      }
+
+      const rule = new Rule([], [], [], [army], orders, (map, board, os) => {})
+      const $ = new RuleHelper(rule)
+
+      rule._resolveOrder = (map, board, os) => {}
+      rule.getErrorMessageForOrder = (map, board, order) => (order.type === 'Hold') ? null : 'invalid'
+      rule.getErrorMessageForOrders = (map, board, orders) => 'eror'
+      rule.defaultOrder = (map, board, unit) => $.A(unit.location).hold()
+
+      const result = rule.resolve(null, { units: [] }, [$.A(mar).move(spa), $.A(mar).hold()]);
+
+      (result == null).should.equal(true)
     })
   })
 })
