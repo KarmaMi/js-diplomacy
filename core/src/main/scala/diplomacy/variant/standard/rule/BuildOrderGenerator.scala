aa31410cadb7571a7c1e1cb339eecfdce29e4e13
karmami <karmami674+github@gmail.com>
2017-01-16 14:36:27 +0900

    Implement BuildOrderGenerator
      ref #17

---
+++ core/src/main/scala/diplomacy/variant/standard/rule/BuildOrderGenerator.scala
@@ -0,0 +1,32 @@
+package diplomacy.variant.standard.rule
+
+import diplomacy.rule.InvalidOrderMessage
+
+class BuildOrderGenerator extends OrderGenerator {
+  def ordersToSkipPhase(board: Board): Option[Set[Order]] = {
+    if (board.map.powers forall { power =>
+      val numOfUnits = board.units count { unit => unit.power == power }
+      val numOfSupply = board.numberOfSupplyCenters.getOrElse(power, 0)
+
+      (numOfUnits == numOfSupply) || (numOfSupply == 0)
+    }) {
+      Option(
+        board.map.powers flatMap { power =>
+          val numOfUnits = board.units count { unit => unit.power == power }
+          val numOfSupply = board.numberOfSupplyCenters.getOrElse(power, 0)
+
+          if (numOfSupply == 0) {
+            board.units withFilter { unit => unit.power == power } map { unit =>
+              Order.Disband(unit): Order
+            }
+          } else {
+            Set[Order]()
+          }
+        }
+      )
+    } else {
+      None
+    }
+  }
+  def defaultOrder(board: Board)(unit: DiplomacyUnit): Order = ??? // TODO
+}
---
+++ core/src/test/scala/diplomacy/variant/standard/rule/BuildOrderGeneratorSpec.scala
@@ -0,0 +1,68 @@
+package diplomacy.variant.standard.rule
+
+import diplomacy.UnitSpec
+import diplomacy.board._
+import diplomacy.variant.standard.map
+import diplomacy.variant.standard.map._
+
+class BuildOrderGeneratorSpec extends UnitSpec {
+  val generator = new BuildOrderGenerator {
+    type Turn = map.Turn
+    type Power = map.Power
+  }
+
+  "A movement-order-generator"  when {
+    "there are no powers that can build or disband units" should {
+      "use an empty set to skip the build phase." in {
+        val board = {
+          Board[State[map.Turn], Power.Power, MilitaryBranch.MilitaryBranch, UnitStatus.UnitStatus, ProvinceStatus.ProvinceStatus](
+            map.map, 1901.Spring - Build,
+            Set(), Map(), Map(), Map()
+          )
+        }
+        val $ = StandardRuleOrderHelper(board)
+        import $._
+
+        generator.ordersToSkipPhase(board) should be(Some(Set()))
+      }
+    }
+
+    "all disband orders are automatically decidable" should {
+      "use disband orders to skip the build phase." in {
+        val board = {
+          Board[State[map.Turn], Power.Power, MilitaryBranch.MilitaryBranch, UnitStatus.UnitStatus, ProvinceStatus.ProvinceStatus](
+            map.map, 1901.Spring - Build,
+            Set(
+              DiplomacyUnit(Germany, Army, Ruh)
+            ), Map(), Map(), Map()
+          )
+        }
+        val $ = StandardRuleOrderHelper(board)
+        import $._
+
+        generator.ordersToSkipPhase(board) should be(Some(Set(A(Ruh).disband())))
+      }
+    }
+
+
+    "otherwise" can {
+      "not skip the build phase." in {
+        val board =
+          Board[State[map.Turn], Power.Power, MilitaryBranch.MilitaryBranch, UnitStatus.UnitStatus, ProvinceStatus.ProvinceStatus](
+            map.map,
+            1901.Autumn - Build,
+            Set(
+              DiplomacyUnit(Germany, Army, Mun),
+              DiplomacyUnit(Italy, Fleet, Nap)
+            ),
+            Map(
+              Rom.province -> Italy, Nap.province -> Italy,
+              Mar.province -> France
+            ),
+            Map(), Map()
+          )
+        generator.ordersToSkipPhase(board) should be(None)
+      }
+    }
+  }
+}
