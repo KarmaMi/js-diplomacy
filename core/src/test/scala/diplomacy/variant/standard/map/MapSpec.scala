43a26b5407c90a98b6ad32f76a9a8e829d227185
karmami <karmami674+github@gmail.com>
2017-01-17 19:17:35 +0900

    Fix MapSpec
      ref #17

--- core/src/test/scala/diplomacy/variant/standard/map/MapSpec.scala
+++ core/src/test/scala/diplomacy/variant/standard/map/MapSpec.scala
@@ -1,38 +1,37 @@
 package diplomacy.variant.standard.map
 
 import diplomacy.UnitSpec
-import diplomacy.board.DiplomacyMapHelper
+import diplomacy.variant.standard.rule._
 
 class MapSpec extends UnitSpec {
-  val $ = DiplomacyMapHelper(map)
   "A map instance" should {
     "define provinces (1)" in {
       Pic.province.isSupplyCenter should be(false)
-      Pic.province.homeOf should be(Option($.p.France))
+      Pic.province.homeOf should be(Option(France))
 
       Bre.province.isSupplyCenter should be(true)
-      Bre.province.homeOf should be(Option($.p.France))
+      Bre.province.homeOf should be(Option(France))
 
       Bur.province.isSupplyCenter should be(false)
-      Bur.province.homeOf should be(Option($.p.France))
+      Bur.province.homeOf should be(Option(France))
 
       Gas.province.isSupplyCenter should be(false)
-      Gas.province.homeOf should be(Option($.p.France))
+      Gas.province.homeOf should be(Option(France))
 
-      map.movableLocationsOf(Par, $.m.A) should be(Set(Pic, Bre, Bur, Gas))
+      map.movableLocationsOf(Par, Army) should be(Set(Pic, Bre, Bur, Gas))
     }
     "define provinces (2)" in {
       Iri.province.isSupplyCenter should be(false)
       Iri.province.homeOf should be(None)
 
-      Wal.province.homeOf should be(Option($.p.England))
+      Wal.province.homeOf should be(Option(England))
 
-      map.movableLocationsOf(Eng, $.m.F) should be(
+      map.movableLocationsOf(Eng, Fleet) should be(
         Set(Iri, Wal, Lon, Nth, Bel, Pic, Bre, Mid)
       )
     }
     "define provinces (3)" in {
-      map.movableLocationsOf(Rom, $.m.F) should be(Set(Tus, Nap, Tyn))
+      map.movableLocationsOf(Rom, Fleet) should be(Set(Tus, Nap, Tyn))
     }
   }
 }
