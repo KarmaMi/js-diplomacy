852ed1d0f6d154b79cab55e91f9a5ee3597fc400
karmami <karmami674+github@gmail.com>
2017-02-06 17:34:25 +0900

    Add browserify task
      ref #32

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp')
 const mocha = require('gulp-mocha')
 const gutil = require('gulp-util')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
 
 gulp.task('mocha', () => {
   return gulp.src(['test/**/*.js'], { read: false })
@@ -9,3 +11,11 @@ gulp.task('mocha', () => {
 })
 
 gulp.task('watch-mocha', () => gulp.watch(['lib/**', 'test/**'], ['mocha']))
+
+gulp.task('browserify', () => {
+  browserify({entries: ['browser/index.js']})
+  .bundle()
+  .pipe(source('diplomacy.js'))
+  .pipe(gulp.dest('browser/'))
+})
+gulp.task('watch-browserify', () => gulp.watch(['lib/**', 'browser/index.js'], ['browserify']))
