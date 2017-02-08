75b8cc5a3c127d2b346fc07e949062237d4ce255
karmami <karmami674+github@gmail.com>
2017-02-08 19:18:27 +0900

    Add gulp task to generate JSDoc
      ref #34

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const mocha = require('gulp-mocha')
 const gutil = require('gulp-util')
 const browserify = require('browserify')
 const source = require('vinyl-source-stream')
+const jsdoc = require('gulp-jsdoc3')
 
 gulp.task('mocha', () => {
   return gulp.src(['test/**/*.js'], { read: false })
     .pipe(mocha({ reporter: 'list' }))
     .on('error', gutil.log)
 })
-
 gulp.task('watch-mocha', () => gulp.watch(['lib/**', 'test/**'], ['mocha']))
 
 gulp.task('browserify', () => {
@@ -19,3 +19,9 @@ gulp.task('browserify', () => {
   .pipe(gulp.dest('browser/'))
 })
 gulp.task('watch-browserify', () => gulp.watch(['lib/**', 'browser/index.js'], ['browserify']))
+
+gulp.task('docs', (cb) => {
+  const configs = require('./configs/jsdoc-config.json')
+  gulp.src(['./lib/**/*.js', 'README.md'], { read: false })
+    .pipe(jsdoc(configs, cb))
+})
