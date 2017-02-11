27b0f7e3da33ac930a9a7ace5a67376842839621
karmami <karmami674+github@gmail.com>
2017-02-11 14:51:49 +0900

    Add a gulp task to create module files
      ref #53

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
+const fs = require('fs')
+const path = require('path')
+
 const gulp = require('gulp')
 const gutil = require('gulp-util')
 const source = require('vinyl-source-stream')
+const through = require('through2')
 
 const ts = require('gulp-typescript')
 const typedoc = require('gulp-typedoc')
@@ -13,8 +17,61 @@ const browserify = require('browserify')
 const tsSourceProject = ts.createProject('./configs/tsconfig.json')
 const tsTestProject = ts.createProject('./configs/tsconfig.json')
 
+gulp.task('create-module-file', () => {
+  return gulp.src(['./src/**'])
+    .pipe(
+      through.obj((file, encoding, callback) => {
+        if (!file.path) {
+          callback()
+          return
+        }
+
+        // It file is not directory, do nothing
+        if (!fs.statSync(file.path).isDirectory()) {
+          callback()
+          return
+        }
+
+        const modulePath = path.resolve(file.path, 'module.ts')
+
+        let importText = ''
+        let exportText = ''
+
+        const children = fs.readdirSync(file.path, encoding)
+        children.forEach(x => {
+          if (x === 'module.ts') return
+
+          let moduleName = path.basename(x, '.ts').toString()
+          let fileName = path.basename(x, '.ts')
+
+          if (fs.statSync(path.resolve(file.path, x)).isDirectory()) {
+            fileName = path.relative(file.path, path.resolve(file.path, x, 'module'))
+          } else {
+            moduleName = path.basename(x, '.ts').toString()
+              .replace(/(^|-)(.)/g, l => l.toUpperCase()).replace(/\W/g, '')
+          }
+
+          importText += `import ${moduleName} from "./${fileName}";\n`
+          exportText += `  ${moduleName}: ${moduleName},\n`
+        })
+
+        const text =
+          `${importText}\n` +
+          `const module = {\n` +
+          `${exportText.substr(0, exportText.length - 2)}\n` +
+          `};\n` +
+          `export default module;`
+
+        const wStream = fs.createWriteStream(modulePath, { defaultEncoding: encoding })
+        wStream.write(text)
+        wStream.end()
+        callback(null, wStream)
+      })
+    )
+})
+
 // compile source files
-gulp.task('compile-src', () => {
+gulp.task('compile-src', ['create-module-file'], () => {
   return gulp.src(['./src/**/*.ts'])
     .pipe(sourcemaps.init())
     .pipe(tsSourceProject(ts.reporter.defaultReporter()))
@@ -38,7 +95,7 @@ gulp.task('test', ['compile-src', 'compile-test'], () => {
 gulp.task('watch-test', () => gulp.watch(['src/**/*.ts', 'test/**/*.ts'], ['test']))
 
 // Create a documentation
-gulp.task('docs', () => {
+gulp.task('docs', ['create-module-file'], () => {
   const configs = require('./configs/tsconfig.json').compilerOptions
   const packageOption = require('./package.json')
   configs.out = './docs'
